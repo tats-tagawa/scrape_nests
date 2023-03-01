9ea85bf3f8e89d66c13337e00b5221788855b924
Tats <10057207+tats-tagawa@users.noreply.github.com>
2023-02-28 20:58:26 -0800

    Add getting urls for all birds from Audubon
    Audubon provides more information in a single page, requiring less pages to scrape.

--- scrape_nests.py
+++ scrape_nests.py
@@ -1,9 +1,32 @@
 import requests
+import re
 import csv
 from bs4 import BeautifulSoup
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
 headers = {'User-Agent':'Mozilla/5.0'}
+
+def get_bird_urls_audubon():
+    # Use page_num = 0 to get all birds
+    page_num = 24
+    bird_urls = []
+
+    while (True):
+        URL = f'https://www.audubon.org/bird-guide?page={page_num}&search_api_views_fulltext=&field_bird_family_tid=All&field_bird_region_tid=All'
+        page = requests.get(URL, headers=headers)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        # Stop loop if there are no more birds to load
+        if soup.body.find_all(string=re.compile('No birds matching')):
+            break
+        birds = soup.find_all('article','bird-card')
+
+        # Delete the first bird as it is the same "Featured Bird" on every page
+        del birds[0]
+
+        for bird in birds:
+            bird_urls.append(bird.a.get('href'))
+        page_num += 1
+    return bird_urls
 
 def get_bird_urls():
     # Use second URL when testing, as it is much smaller list
@@ -45,8 +68,8 @@ def write_to_csv(birds):
         for bird in birds:
             writer.writerow(bird)
 
-if __name__ == '__main__':
-    bird_urls = get_bird_urls()
-    birds_data = get_all_bird_data(bird_urls)
-    write_to_csv(birds_data)
-
+# if __name__ == '__main__':
+#     bird_urls = get_bird_urls_ebird()
+#     birds_data = get_all_bird_data(bird_urls)
+#     write_to_csv(birds_data)
+print(get_bird_urls_audubon())
