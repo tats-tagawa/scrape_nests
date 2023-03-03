91d8f33d757e01b500ef08a5356ddf537b9bed57
Tats <10057207+tats-tagawa@users.noreply.github.com>
2023-03-02 23:12:41 -0800

    Remove code to scrape from eBird

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Scrape Nests
 
-Scrape bird info from the [Bird Guide](https://www.allaboutbirds.org/guide/browse/taxonomy/) created by [The Cornell Lab of Ornithology](https://www.birds.cornell.edu/home/). Hobby project to get familiar with Python programming language (snakes are cool too).
-
-Note: In process of moving to [Guide to North American Birds](https://www.audubon.org/bird-guide) created by [National Audubon Society](https://www.audubon.org/) for data.
+Scrape bird info from the [Guide to North American Birds](https://www.audubon.org/bird-guide) created by [National Audubon Society](https://www.audubon.org/) . Hobby project to get familiar with Python programming language (snakes are cool too).
 
 Please consider donating to [The Cornell Lab of Ornithology](https://give.birds.cornell.edu/page/87895/donate/) and / or [National Audubon Society](https://act.audubon.org/a/donate?ms=digital-fund-web-website_nas-topmenu_donate_202107) to protect our birds.
 
--- scrape_nests.py
+++ scrape_nests.py
@@ -57,31 +57,6 @@ def get_bird_data_audubon(url):
 
     return [name, scientific_name, description, conservation, family, habitat]
 
-def get_bird_urls_ebird():
-    # Use second URL when testing, as it is much smaller list
-    URL = 'https://www.allaboutbirds.org/guide/browse/taxonomy'
-    # URL = 'https://www.allaboutbirds.org/guide/browse/taxonomy/Podicipedidae'
-    page = requests.get(URL, headers=headers)
-
-    soup = BeautifulSoup(page.content, 'html.parser')
-    birds = soup.find_all('div','species-info')
-    bird_urls = []
-    for bird in birds:
-        bird_url = 'https://www.allaboutbirds.org' + bird.a.get('href')
-        bird_urls.append(bird_url)
-    return bird_urls
-
-def get_bird_data_ebird(url):
-    bird_page = requests.get(url, headers=headers)
-    soup = BeautifulSoup(bird_page.content, 'html.parser')
-    species_info = soup.find('div','speciesInfoCard')
-
-    name = species_info.span.get_text()
-    scientific_name = species_info.em.get_text()
-    description = species_info.p.get_text()
-
-    return [name, scientific_name, description]
-
 def get_all_bird_data(urls):
     with ProcessPoolExecutor(max_workers=4) as executor:
         futures = [executor.submit(get_bird_data_audubon, url) for url in urls]
